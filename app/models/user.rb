5cd2f5296d61fa8472f136d52e3db8952722be41
Huan Nguyen <huannguyen@freec.asia>
2023-02-18 11:25:32 +0700

    email validatation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,22 @@
 # User
 class User < ApplicationRecord
+  # == Extensions ===========================================================
   extend Enumerize
+
+  # == Relationships ========================================================
+  has_many :movies
+
+  # == Validations ==========================================================
   validates :email, uniqueness: true
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :password, presence: true
-  has_many :movies
+
+  # == Custom validations =================
+  validate :email_validation
+
+  # == Instant Methods ========================================================
+  def email_validation
+    email_service = EmailVerifyService.new(email, 'power')
+    errors.add(:email, email_service.message(true)) if !email_service.valid
+  end
 end
