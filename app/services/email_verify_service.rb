6bba3f35f9b04dc7d4ed6021dc8c8a213f8c4744
Huan Nguyen <huannguyen@freec.asia>
2023-02-18 09:19:20 +0700

    add email verify service

---
+++ app/services/email_verify_service.rb
@@ -0,0 +1,35 @@
+class EmailVerifyService
+  def initialize(email, mode) # mode = quick || power
+    @email = email
+    @mode = mode
+  end
+
+  def valid
+    return true if ['false', false, nil].include?(ENV['VALIDATE_EMAIL']) # bypass email verify service
+    begin
+      response = HTTParty.get("https://emailverifier.reoon.com/api/v1/verify?email=#{@email}&key=#{api_key}&mode=#{@mode}").parsed_response
+      response['status'] == 'safe'
+    rescue => e
+      Sentry.capture_exception(e)
+    end
+  end
+
+  def message(english)
+    valid ? valid_message(english) : invalid_message(english)
+  end
+
+  private
+
+  def invalid_message(english)
+    english ? 'This email does not exist, please check again!' : 'Email này không tồn tại, vui lòng kiểm tra lại!'
+  end
+
+  def valid_message(english)
+    english ? 'This is valid email' : 'Email hợp lệ'
+  end
+
+  def api_key
+    ENV['EMAIL_VERIFIER_TOKEN']
+  end
+
+end
