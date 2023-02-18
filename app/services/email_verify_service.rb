a17a588ca4d381bb0f926e9b223291dd4e7170a2
Huan Nguyen <huannguyen@freec.asia>
2023-02-18 09:20:25 +0700

    push message to sendtry

--- app/services/email_verify_service.rb
+++ app/services/email_verify_service.rb
@@ -8,6 +8,7 @@ class EmailVerifyService
     return true if ['false', false, nil].include?(ENV['VALIDATE_EMAIL']) # bypass email verify service
     begin
       response = HTTParty.get("https://emailverifier.reoon.com/api/v1/verify?email=#{@email}&key=#{api_key}&mode=#{@mode}").parsed_response
+      ::Sentry.capture_message("SENDING EMAIL BY EMAIL VERIFY: #{response['status']}") if response['status'] == 'error'
       response['status'] == 'safe'
     rescue => e
       Sentry.capture_exception(e)
