e2ab7b7f28e0f5cee0c60d011df26058651b1eff
Huan Nguyen <huannguyen@freec.asia>
2023-03-11 10:55:17 +0700

    fix verify email service

--- app/services/email_verify_service.rb
+++ app/services/email_verify_service.rb
@@ -7,13 +7,13 @@ class EmailVerifyService
 
   def valid
     return true if ['false', false, nil].include?(ENV['VALIDATE_EMAIL']) # bypass email verify service
+    ::Sentry.capture_message("MISSING API KEY EMAIL VERIFY") unless ENV['EMAIL_VERIFIER_TOKEN'].present?
     begin
       response = HTTParty.get("https://emailverifier.reoon.com/api/v1/verify?email=#{@email}&key=#{api_key}&mode=#{@mode}").parsed_response
       if response['status'] == 'error' # out of credit service
         ::Sentry.capture_message("SENDING EMAIL BY EMAIL VERIFY: #{response['status']}")
         return true
       end
-      ::Sentry.capture_message("MISSING API KEY EMAIL VERIFY") unless ENV['EMAIL_VERIFIER_TOKEN'].present?
       response['status'] == 'safe'
     rescue => e
       ::Sentry.capture_exception(e)
