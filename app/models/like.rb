44fce9f809d22c814ab21037a2370ac8931d42e4
Huan Nguyen <huannguyen@freec.asia>
2023-03-11 16:14:43 +0700

    improve like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,8 +1,17 @@
 class Like < ApplicationRecord
-  belongs_to :movie
-  belongs_to :user
+  # == Extensions ===========================================================
   enum status: { dislike: 0, like: 1 }
+
+  # == Validations ==========================================================
   validates_uniqueness_of :user_id, scope: :movie_id
+  validates :status, uniqueness: true
+  validates :status, presence: true
+
+  # == Scopes ===============================================================
   scope :likes, -> { where(status: 1) }
   scope :dislikes, -> { where(status: 0) }
+
+  # == Relationships ========================================================
+  belongs_to :movie
+  belongs_to :user
 end
