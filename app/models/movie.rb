f22c48aebe9d8f2cc9dbe90819eb735c5c98b142
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-03-28 15:47:15 +0700

    annotate models

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: movies
+#
+#  id          :integer          not null, primary key
+#  user_id     :integer
+#  title       :string
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  embed_url   :string
+#
 class Movie < ApplicationRecord
   include Filterable
   belongs_to :user
--- spec/factories/likes.rb
+++ spec/factories/likes.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  movie_id   :integer
+#  user_id    :integer
+#  like_type  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  status     :integer
+#
 FactoryGirl.define do
   factory :like do
     movie nil
