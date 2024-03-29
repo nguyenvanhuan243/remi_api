1aeb4c6bc40b3e35e38b252f60d8dacdaf3d314e
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-03-29 13:45:00 +0700

    Integrate sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,12 +1,13 @@
 Sentry.init do |config|
-  config.dsn = 'https://493db188648740d7b8a7037cf5faf9d2@o4504698737983488.ingest.sentry.io/4504698738966528'
+  config.dsn = 'https://b8e8b651129eff2369a8b443ea2e4af4@o362235.ingest.us.sentry.io/4506992930258944'
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
   # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
   # or
-  config.traces_sampler = lambda do |_context|
+  config.traces_sampler = lambda do |context|
     true
   end
 end
