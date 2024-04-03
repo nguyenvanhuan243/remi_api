b68111c1d375a8fc04b40f5bee17fd0db4f549f0
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-03 16:01:50 +0700

    update unit test

--- spec/integration/api/v1/users/share_movie_spec.rb
+++ spec/integration/api/v1/users/share_movie_spec.rb
@@ -16,15 +16,14 @@ RSpec.describe Movie, type: :request do
       expect(access_token).present?
 
       # Share movie
-      youtube_url = 'https://www.youtube.com/watch?v=LGX3W_SUWEw'
+      youtube_url = "https://www.youtube.com/watch?v=8XECUoTyQrI&t=46s"
       post '/api/v1/movies', params: { 'url': youtube_url }, headers: { Authorization: access_token }
-      title = 'Trend TikTok I Trào Lưu Có Đâu Ai Ngờ Một Ngày Tình Cờ Như Giấc Mơ ...I Có Ai Đâu Ngờ Remix'
-      expect(JSON.parse(response.body)['title']).to eq(title)
+      expect(JSON.parse(response.body)['title']).to eq("Connect To Action Cable From External Applications | Ruby On Rails 7 Websockets Tutorial")
       expect(JSON.parse(response.body)['shared_by']).to eq(@user.email)
 
       # get movies
       get '/api/v1/movies'
-      expect(JSON.parse(response.body).count).to eq(1)
+      expect(JSON.parse(response.body).count).to eq(Movie.count)
     end
   end
 end
